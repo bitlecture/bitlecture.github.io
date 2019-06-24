199e76e2004a2f049891687057f8c5eaeb521ab2
shinianzhihou <1178396201@qq.com>
2019-06-24 13:11:02 +0800

    整理返回頂部

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -132,22 +132,6 @@ $(document).ready(function() {
     closeOnContentClick: true,
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
-  
-  //backtotop
-$(window).scroll(function() {
-  var backToTopButton = $(".back-to-top");
-  if ($(window).scrollTop() >= 200) {
-    backToTopButton.css("bottom", "0px");
-  } else {
-    backToTopButton.attr('style', '');
-  }
-  backToTopButton.click(function() {
-    $('body,html').stop().animate({
-      scrollTop: 0
-    }, "slow");
-  });
-  return false;
-});
 
 });
 
